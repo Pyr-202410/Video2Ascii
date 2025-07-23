825bc7614fdc1819d09646fd38795f6d324e7d75
Pyr-202410 <262958551@qq.com>
2025-07-23 11:59:37 +0800

    Delete Video2Ascii directory

--- Video2Ascii/setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-from Cython.Build import cythonize
-import numpy as np
-setup(
-    name='V2A',
-    ext_modules=cythonize("Video2Ascii.pyx"),
-    include_dirs=[np.get_include()],  # 添加这一行
-    requires=['Cython', 'numpy', 'cv2', 'Pillow', 'tqdm']
-)
